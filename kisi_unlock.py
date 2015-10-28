78962e7a408e877854a3f7b50c268c7607d3a0e6
jessejoe <jesse@jessejoe.com>
2015-10-28 12:07:03 -0400

    Add script to use KISI API to unlock a lock

---
+++ kisi_unlock.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import logging
+import argparse
+import getpass
+
+from api import KisiApi
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Script to unlock a lock using the Kisi API')
+    parser.add_argument('-v',
+                        '--verbose',
+                        action='store_true',
+                        help='verbose logging')
+    parser.add_argument('-e',
+                        '--email',
+                        help='e-mail to login with',
+                        required=True)
+    parser.add_argument(
+        '-p',
+        '--password',
+        help='password to login with, will prompt if not supplied')
+    parser.add_argument('-l',
+                        '--lock',
+                        help='name of lock to unlock (partial match supported)',
+                        required=True)
+    args = parser.parse_args()
+
+    password = args.password
+    if not password:
+        password = getpass.getpass()
+
+    if args.verbose:
+        loglevel = logging.DEBUG
+    else:
+        loglevel = logging.INFO
+
+    logging.basicConfig(level=loglevel,
+                        format='%(asctime)s %(name)s %(levelname)s %(message)s')
+    # requests default INFO logs are noisy
+    logging.getLogger("requests").setLevel(logging.WARNING)
+
+    api = KisiApi()
+    api.login(args.email, password)
+    api.unlock(args.lock)
+
+
+if __name__ == "__main__":
+    main()
